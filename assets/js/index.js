930eb019f549e5dbc9b91671f857833439876296
Abbas-Davodkhani <davodkhaniabbas2003@gmail.com>
2022-07-25 02:54:20 +0430

    new version change 2

---
+++ assets/js/index.js
@@ -0,0 +1,94 @@
+$(document).ready(function () {
+    $(".btn-saturday").click(function () {
+        
+        $(".saturday").css('display', 'block');
+        
+        $(".sunday").css('display', 'none');
+        $(".monday").css('display', 'none');
+        $(".thursday").css('display', 'none');
+        $(".wendesday").css('display' , 'none');
+        $(".thusday").css('display' , 'none');
+        $(".friday").css('display' , 'none');
+
+    });
+
+    //
+    $(".btn-sunday").click(function () {
+
+        $(".sunday").css('display', 'block');
+        
+        $(".saturday").css('display', 'none');
+        $(".monday").css('display', 'none');
+        $(".thursday").css('display', 'none');
+        $(".wendesday").css('display' , 'none');
+        $(".thusday").css('display' , 'none');
+        $(".friday").css('display' , 'none');
+
+    });
+    //
+    $(".btn-monday").click(function () {
+       
+
+        $(".monday").css('display', 'block');
+        
+        $(".sunday").css('display', 'none');
+        $(".saturday").css('display', 'none');
+        $(".thursday").css('display', 'none');
+        $(".wendesday").css('display' , 'none');
+        $(".thusday").css('display' , 'none');
+        $(".friday").css('display' , 'none');
+
+    });
+    //
+    $(".btn-thursday").click(function () {
+  
+        $(".thursday").css('display', 'block');
+        
+        $(".sunday").css('display', 'none');
+        $(".monday").css('display', 'none');
+        $(".saturday").css('display', 'none');
+        $(".wendesday").css('display' , 'none');
+        $(".thusday").css('display' , 'none');
+        $(".friday").css('display' , 'none');
+
+    });
+    //
+    $(".btn-wendesday").click(function () {
+        
+        $(".wendesday").css('display', 'block');
+        
+        $(".sunday").css('display', 'none');
+        $(".monday").css('display', 'none');
+        $(".thursday").css('display', 'none');
+        $(".saturday").css('display' , 'none');
+        $(".thusday").css('display' , 'none');
+        $(".friday").css('display' , 'none');
+
+    });
+    //
+    $(".btn-thusday").click(function () {
+        $(".thusday").css('display', 'block');
+        
+        $(".sunday").css('display', 'none');
+        $(".monday").css('display', 'none');
+        $(".thursday").css('display', 'none');
+        $(".wendesday").css('display' , 'none');
+        $(".saturday").css('display' , 'none');
+        $(".friday").css('display' , 'none');
+
+    });
+    //
+    $(".btn-friday").click(function () {
+
+        $(".friday").css('display', 'block');
+        
+        $(".sunday").css('display', 'none');
+        $(".monday").css('display', 'none');
+        $(".thursday").css('display', 'none');
+        $(".wendesday").css('display' , 'none');
+        $(".thusday").css('display' , 'none');
+        $(".saturday").css('display' , 'none');
+
+    });
+    //
+});
